a4ef0aa8674ad19c2a2c0ad3b077ede73df3af9d
unknown <khan.afnan1117@gmail.com>
2021-01-16 18:33:22 +0530

    deploying

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config
 const mongoose=require('mongoose')
 
-mongoose.set('debug',true)
+//mongoose.set('debug',true)
 mongoose.Promise=global.Promise;
 console.log("ffffff+   "+process.env.URL)
 mongoose.connect(process.env.URL)

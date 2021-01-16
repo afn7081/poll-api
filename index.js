81c58e6e84753eef81575799f461a814ce58b831
unknown <khan.afnan1117@gmail.com>
2021-01-16 19:38:32 +0530

    adder cross origin support

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const obj={
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin,'Authorization', X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin,authorization, X-Requested-With, Content-Type, Accept");
     next();
   });
 
